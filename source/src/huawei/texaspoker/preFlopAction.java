898889ba98938e7b9f2536d90f69c179d4894c2a
nju525 <zjgtan@163.com>
2015-05-27 20:24:37 +0800

    Update preFlopAction.java

--- source/src/huawei/texaspoker/preFlopAction.java
+++ source/src/huawei/texaspoker/preFlopAction.java
@@ -171,7 +171,7 @@ public class preFlopAction {
 				if (myRestJetton < 4 * BB) {
 					return "all_in";
 				} else {
-					if (bet >=BB) {
+					if (bet >0) {
 						return "fold";
 					} else {
 						return "check";// 大盲位check
@@ -189,7 +189,7 @@ public class preFlopAction {
 				if (myRestJetton <= 2 * BB) {
 					return "all_in";
 				} else {
-					if (bet > BB) {
+					if (bet > 0) {
 						return "fold";
 					} else {
 						return "check";// 大盲位check
